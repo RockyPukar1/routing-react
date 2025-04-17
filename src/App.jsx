b49b2121c820d69b53b0cbd864765d102ae0d19d
Rocky Pukar <ckrp2018@gmail.com>
2025-04-17 19:37:07 +0545

    open one at a time

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,49 @@
 import { useState } from "react";
 
+const faqs = [
+  {
+    question: "How many bones does a cat have?",
+    answer: "A cat has 230 bones - 6 more than a human",
+  },
+  {
+    question: "How much do cats sleep?",
+    answer: "The average cat sleeps 12-16 hours per day",
+  },
+  {
+    question: "How long do cats live",
+    answer:
+      "Outdoor cats live 5 years on average. Indoor\ncats live 15 years on average.",
+  },
+];
+
 function App() {
-  const [users, setUsers] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  function getAllUsers() {
-    setIsLoading(true);
-    fetch("https://dummyjson.com/users")
-      .then((res) => res.json())
-      .then((data) => {
-        setUsers(data?.users);
-        setTimeout(() => {
-          setIsLoading(false);
-        }, 1000);
-      });
-  }
-
-  console.log(users);
-
-  if (isLoading) {
-    return "Loading";
-  }
-
+  const [showIndex, setShowIndex] = useState(0);
   return (
-    <div>
-      <button className="bg-black text-white" onClick={() => setUsers([])}>
-        Remove Data
-      </button>
-      <button className="bg-red-500 text-blue" onClick={() => getAllUsers()}>
-        Fetch Data
-      </button>
-      {users.map((user) => (
-        <div key={user.id}>
-          <div>
-            Full Name: {user.firstName} {user.lastName}
+    <div className="flex justify-center items-center">
+      <div className="flex-col w-72 text-xs">
+        {faqs.map((faq, index) => (
+          <div className="bg-gray-300 my-2 p-2">
+            <p className="font-semibold flex gap-1 items-center">
+              <img
+                onClick={() => {
+                  if (index === showIndex) {
+                    setShowIndex("");
+                  } else {
+                    setShowIndex(index);
+                  }
+                }}
+                className={`w-5 h-2 ${index === showIndex && "rotate-90"}`}
+                src="/src/assets/chevron.svg"
+                alt="Chevron"
+              />
+              {faq.question}
+            </p>
+            {index === showIndex && (
+              <p className="font-normal ml-6">{faq.answer}</p>
+            )}
           </div>
-          <div>Age: {user.age}</div>
-          <div>Country: {user.address.country}</div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
